26cc2565dce66348fbfa98d79bf9f3b0f7bd3122
mateo.ziegler <mateolziegler@gmail.com>
2026-07-06 22:04:23 +0200

    minor change

--- plugin.php
+++ plugin.php
@@ -2,7 +2,7 @@
 return array(
     'id'          => 'discord',                  		   // Must match folder suffix
     'version'     => '1.0.0',
-    'name'        => 'OsTicket-discord',
+    'name'        => 'OsTicketDiscord',
     'author'      => 'Plunder283',
     'description' => 'Notify Discord on new tickets, replies, status changes, and closures.',
     'url'         => 'https://github.com/Plunder283',
